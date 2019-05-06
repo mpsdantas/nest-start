fd82be2e625dc3ebcda383468185c5a29e7270a3
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2019-05-05 23:45:15 -0300

    Ajuste para o debug.

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Document, Model } from 'mongoose';
 import { UserInterface } from '../interfaces/user.interface';
-import { BaseService } from 'src/modules/shared/services/base.service';
+import { BaseService } from "../../shared/services/base.service";
 
 @Injectable()
 export class UserService extends BaseService<UserInterface>{
